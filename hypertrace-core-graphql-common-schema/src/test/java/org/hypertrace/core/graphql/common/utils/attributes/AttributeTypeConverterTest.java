ac808a07be3db58fef15618b024aeea1b532a72f
Tim Mwangi <tim@traceable.ai>
2020-09-27 13:19:28 -0700

    add some unit tests

---
+++ hypertrace-core-graphql-common-schema/src/test/java/org/hypertrace/core/graphql/common/utils/attributes/AttributeTypeConverterTest.java
@@ -0,0 +1,30 @@
+package org.hypertrace.core.graphql.common.utils.attributes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.reactivex.rxjava3.core.Single;
+import org.hypertrace.core.graphql.attributes.AttributeModelType;
+import org.hypertrace.core.graphql.common.schema.attributes.AttributeType;
+import org.junit.jupiter.api.Test;
+
+public class AttributeTypeConverterTest {
+  @Test
+  void testConvert() {
+    AttributeTypeConverter attributeTypeConverter = new AttributeTypeConverter();
+
+    assertEquals(Single.just(AttributeType.STRING).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.STRING).blockingGet());
+    assertEquals(Single.just(AttributeType.BOOLEAN).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.BOOLEAN).blockingGet());
+    assertEquals(Single.just(AttributeType.LONG).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.LONG).blockingGet());
+    assertEquals(Single.just(AttributeType.DOUBLE).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.DOUBLE).blockingGet());
+    assertEquals(Single.just(AttributeType.TIMESTAMP).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.TIMESTAMP).blockingGet());
+    assertEquals(Single.just(AttributeType.STRING_MAP).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.STRING_MAP).blockingGet());
+    assertEquals(Single.just(AttributeType.STRING_ARRAY).blockingGet(),
+        attributeTypeConverter.convert(AttributeModelType.STRING_ARRAY).blockingGet());
+  }
+}
