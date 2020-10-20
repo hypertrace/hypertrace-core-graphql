856a1f63489cbcd46cf31bc517fb99fdb1259fbf
Aaron Steinfeld <aaron@traceable.ai>
2020-10-20 10:27:20 -0400

    feat: add support for unknown scopes

--- hypertrace-core-graphql-attribute-scope/src/main/java/org/hypertrace/core/graphql/atttributes/scopes/HypertraceCoreAttributeScope.java
+++ hypertrace-core-graphql-attribute-scope/src/main/java/org/hypertrace/core/graphql/atttributes/scopes/HypertraceCoreAttributeScope.java
@@ -3,6 +3,28 @@ package org.hypertrace.core.graphql.atttributes.scopes;
 import org.hypertrace.core.graphql.common.schema.attributes.AttributeScope;
 
 enum HypertraceCoreAttributeScope implements AttributeScope {
-  TRACE,
-  SPAN;
+  TRACE(HypertraceCoreAttributeScopeString.TRACE),
+  SPAN(HypertraceCoreAttributeScopeString.SPAN, HypertraceCoreAttributeScopeString.SPAN_EXTERNAL);
+
+  private final String scope;
+  private final String externalScope;
+
+  HypertraceCoreAttributeScope(String scope) {
+    this(scope, scope);
+  }
+
+  HypertraceCoreAttributeScope(String scope, String externalScope) {
+    this.scope = scope;
+    this.externalScope = externalScope;
+  }
+
+  @Override
+  public String getScopeString() {
+    return this.scope;
+  }
+
+  @Override
+  public String getExternalScopeString() {
+    return this.externalScope;
+  }
 }
---
+++ hypertrace-core-graphql-attribute-scope/src/main/java/org/hypertrace/core/graphql/atttributes/scopes/HypertraceCoreAttributeScopeString.java
@@ -0,0 +1,7 @@
+package org.hypertrace.core.graphql.atttributes.scopes;
+
+public interface HypertraceCoreAttributeScopeString {
+  String SPAN = "EVENT";
+  String SPAN_EXTERNAL = "SPAN";
+  String TRACE = "TRACE";
+}
