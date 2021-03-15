24ecbf88098c16d33f0ea519bcbf4ea750975d4e
Aaron Steinfeld <aaron@traceable.ai>
2021-03-15 08:32:42 -0400

    style: add formatter and apply

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   id("org.hypertrace.jacoco-report-plugin") version "0.1.3" apply false
   id("org.hypertrace.docker-java-application-plugin") version "0.8.1" apply false
   id("org.hypertrace.docker-publish-plugin") version "0.8.1" apply false
+  id("org.hypertrace.code-style-plugin") version "1.0.2" apply false
 }
 
 subprojects {
@@ -14,10 +15,11 @@ subprojects {
       sourceCompatibility = JavaVersion.VERSION_11
       targetCompatibility = JavaVersion.VERSION_11
     }
+
   }
 
   pluginManager.withPlugin("java-library") {
-
+    apply(plugin = "org.hypertrace.code-style-plugin")
     dependencies {
       "api"(platform(project(":hypertrace-core-graphql-platform")))
       "annotationProcessor"(platform(project(":hypertrace-core-graphql-platform")))
