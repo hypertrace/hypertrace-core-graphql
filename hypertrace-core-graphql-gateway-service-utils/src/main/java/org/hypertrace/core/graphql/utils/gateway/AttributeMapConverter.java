4af1393120f8be5b0181bc276578e9ff5b9bd8d4
rish691 <rishabh@traceable.ai>
2021-04-22 22:34:35 +0530

    Use biconverter

--- hypertrace-core-graphql-gateway-service-utils/src/main/java/org/hypertrace/core/graphql/utils/gateway/AttributeMapConverter.java
+++ hypertrace-core-graphql-gateway-service-utils/src/main/java/org/hypertrace/core/graphql/utils/gateway/AttributeMapConverter.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.inject.Inject;
+import org.hypertrace.core.graphql.attributes.AttributeModel;
 import org.hypertrace.core.graphql.common.request.AttributeRequest;
 import org.hypertrace.core.graphql.common.utils.BiConverter;
 import org.hypertrace.core.graphql.common.utils.CollectorUtils;
@@ -16,10 +17,10 @@ import org.hypertrace.gateway.service.v1.common.Value;
 class AttributeMapConverter
     implements BiConverter<Collection<AttributeRequest>, Map<String, Value>, Map<String, Object>> {
 
-  private final Converter<ValueWrapper, Object> valueConverter;
+  private final BiConverter<Value, AttributeModel, Object> valueConverter;
 
   @Inject
-  AttributeMapConverter(Converter<ValueWrapper, Object> valueConverter) {
+  AttributeMapConverter(BiConverter<Value, AttributeModel, Object> valueConverter) {
     this.valueConverter = valueConverter;
   }
 
@@ -37,7 +38,7 @@ class AttributeMapConverter
       AttributeRequest attributeRequest, Map<String, Value> response) {
     // Uses SimpleImmutableEntry to support null values
     return this.valueConverter
-        .convert(new ValueWrapper(response.get(attributeRequest.alias()), attributeRequest))
+        .convert(response.get(attributeRequest.alias()), attributeRequest.attribute())
         .map(value -> new SimpleImmutableEntry<>(attributeRequest.attribute().key(), value));
   }
 }
