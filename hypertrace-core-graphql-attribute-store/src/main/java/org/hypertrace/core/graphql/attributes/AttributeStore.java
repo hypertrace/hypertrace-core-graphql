6e3fbf281c5b4db67e0bc51c061df98f3037d9e6
Aaron Steinfeld <aaron@traceable.ai>
2021-09-01 18:47:08 -0400

    style: spotless

--- hypertrace-core-graphql-attribute-store/src/main/java/org/hypertrace/core/graphql/attributes/AttributeStore.java
+++ hypertrace-core-graphql-attribute-store/src/main/java/org/hypertrace/core/graphql/attributes/AttributeStore.java
@@ -1,9 +1,7 @@
 package org.hypertrace.core.graphql.attributes;
 
 import io.reactivex.rxjava3.core.Single;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import org.hypertrace.core.graphql.context.GraphQlRequestContext;
 
 public interface AttributeStore {
--- hypertrace-core-graphql-attribute-store/src/main/java/org/hypertrace/core/graphql/attributes/AttributeStoreModule.java
+++ hypertrace-core-graphql-attribute-store/src/main/java/org/hypertrace/core/graphql/attributes/AttributeStoreModule.java
@@ -3,7 +3,6 @@ package org.hypertrace.core.graphql.attributes;
 import com.google.inject.AbstractModule;
 import com.google.inject.Key;
 import com.google.inject.multibindings.Multibinder;
-import io.grpc.CallCredentials;
 import io.reactivex.rxjava3.core.Scheduler;
 import org.hypertrace.core.graphql.rx.BoundedIoScheduler;
 import org.hypertrace.core.graphql.spi.config.GraphQlServiceConfig;
