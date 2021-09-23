cbf23ed1003f81616c9b40795c725fc3fdbda41d
Ankit Choudhary <ankitchoudhary@192.168.2.103>
2021-09-23 21:44:52 +0530

    same version for netty libs

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     runtime("io.netty:netty-codec-http2:4.1.68.Final") {
       because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1083991")
     }
-    runtime("io.netty:netty-handler-proxy:4.1.61.Final") {
+    runtime("io.netty:netty-handler-proxy:4.1.68.Final") {
       because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1083991")
     }
   }
