c756a418f669711897c4d88bd3dc00b92cecd0fc
Aaron Steinfeld <aaron@traceable.ai>
2022-10-17 21:09:41 -0400

    refactor: update grpc utils

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -10,10 +10,10 @@ dependencies {
   api(platform("io.grpc:grpc-bom:1.47.0"))
   constraints {
 
-    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.0")
+    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.2")
     api("org.hypertrace.gateway.service:gateway-service-api:0.2.15")
     api("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.14.8")
 
@@ -23,7 +23,6 @@ dependencies {
     api("org.slf4j:slf4j-api:1.7.36")
     api("io.reactivex.rxjava3:rxjava:3.1.5")
     api("com.google.protobuf:protobuf-java-util:3.21.7")
-    api("com.google.protobuf:protobuf-java:3.21.7")
 
     api("org.projectlombok:lombok:1.18.24")
     api("com.google.code.findbugs:jsr305:3.0.2")
