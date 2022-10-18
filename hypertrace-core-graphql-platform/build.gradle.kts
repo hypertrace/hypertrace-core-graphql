eb3c24a588c41417af95f5a882140965255cdc78
Aaron Steinfeld <aaron@traceable.ai>
2022-10-17 21:16:02 -0400

    refactor: update commons text too

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.4")
     api("com.fasterxml.jackson:jackson-bom:2.13.4")
-    api("org.apache.commons:commons-text:1.9")
+    api("org.apache.commons:commons-text:1.10.0")
     api("io.opentelemetry:opentelemetry-proto:1.1.0-alpha")
 
     runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
