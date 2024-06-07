ce2236086ae9903e64b562a0dfe3a035901da95c
kishansairam9 <kishan.adapa@traceable.ai>
2024-06-07 19:02:42 +0530

    update catalog version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 
 plugins {
   id("org.hypertrace.version-settings") version "0.2.1"
-  id("org.hypertrace.dependency-settings") version "0.1.1"
+  id("org.hypertrace.dependency-settings") version "0.1.2"
 }
 
 configure<DependencyPluginSettingExtension> {
